c572cf93e19ea91da7e0c7645ff25855698b2395
matt.vrtis <matt.vrtis@gmail.com>
2026-03-31 10:07:44 -0400

    fixing deploy link structure

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  base: (process.env.BASE_PATH ?? '/').replace(/([^/])$/, '$1/'),
   integrations: [react(), vue(), mdx()],
   outDir: './dist',
   publicDir: './public',
